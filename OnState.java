e80280310caa77d1e5907f6d16228a03d59c8c77
1505039 <tanvirmahmud201505039@gmail.com>
2018-06-30 20:12:41 +0600

    implements State interface

---
+++ OnState.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpackage;
+
+/**
+ *
+ * @author tanvir
+ */
+public class OnState implements State{
+    String def;
+    @Override
+    public void action(EditState estate, String act) {
+      
+         
+        def=act;
+        estate.setState(this);
+    
+    
+    }
+    
+    
+    
+    @Override
+    public void result() {
+      
+       System.out.println(def);
+
+    }
+   
+  
+
+   
+}
